29f767b2535e8cdad06a822573a3479b67a8e4a1
kyli6230 <kyli6230@ISS2TTQKH2.scdsb.pri>
2020-09-15 11:54:49 -0400

    Updated Barchart file

--- src/BarChart.java
+++ src/BarChart.java
@@ -1,9 +1,21 @@
 import TurtleGraphics.*;
+import java.util.Scanner;
 
 public class BarChart {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        int value1, value2, value3, value4;
+        
+        Scanner s = new Scanner(System.in);
+        //ask for pizza size and number of toppings
+        System.out.print("Enter value 1 of 4 -> ");
+        value1 = s.nextInt();
+        System.out.print("Enter value 2 of 4 -> ");
+        value2 = s.nextInt();
+        System.out.print("Enter value 3 of 4 -> ");
+        value3 = s.nextInt();
+        System.out.print("Enter value 4 of 4 -> ");
+        value4 = s.nextInt();
     }
     
 }
