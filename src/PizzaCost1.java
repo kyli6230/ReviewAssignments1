c59e3ba69628bab271bf1784b12c59ae0bf461c6
kyli6230 <kyli6230@ISS2TTQKH2.scdsb.pri>
2020-09-15 11:42:03 -0400

    Initial commit

---
+++ src/PizzaCost1.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+
+public class PizzaCost1 {
+
+    public static void main(String[] args) {
+        double size, total;
+        int toppings;
+        double labour = 1.5, gas = 0.75;
+        
+        Scanner s = new Scanner(System.in);
+        //ask for pizza size and number of toppings
+        System.out.print("Enter pizza size (in inches) -> ");
+        size = s.nextDouble();
+        System.out.print("Enter number of toppings -> ");
+        toppings = s.nextInt();
+        //calculate total
+        total = labour + gas + (0.75 * toppings) + (0.50 * size);
+        //print the answer
+        System.out.println(String.format("Total cost is %.2f",total));
+    }
+    
+}
